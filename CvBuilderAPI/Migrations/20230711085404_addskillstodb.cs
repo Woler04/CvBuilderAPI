d9a44b4507eb90b9e75b50e1c791d3b6c98796dc
Woler <hristo_den@abv.bg>
2023-07-11 11:54:49 +0300

    fixed

---
+++ CvBuilderAPI/Migrations/20230711085404_addskillstodb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CvBuilderAPI.Migrations
+{
+    public partial class addskillstodb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
